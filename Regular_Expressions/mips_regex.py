73a06a33b036c786de3cbe39a7554adabd7db12a
Youssef-Remah <yremah8@gmail.com>
2023-11-28 19:29:26 +0200

    Added identifier, and register regex

--- Regular_Expressions/mips_regex.py
+++ Regular_Expressions/mips_regex.py
@@ -15,3 +15,12 @@ left_paran_regex = re.compile(pattern = r'^\(+$')
 
 right_paran_regex = re.compile(pattern = r'^\)+$')
 
+# 1) String starts with a letter from a-z or A-Z, and all other chars are a-z, A-Z, 0-9, or _
+identifier_regex = re.compile(r'^[a-zA-Z][a-zA-Z0-9_]*$')
+
+# 2) String starts with a dot and the other chars are a-z or A-Z
+dot_identifier_regex = re.compile(r'^\.[a-zA-Z]+$')
+
+# 3) String starts with $ dollar sign, the second char is a letter from a-z or A-Z,
+# and the third char is a number 0-9
+register_regex = re.compile(r'^\$[a-zA-Z][0-9]$')
